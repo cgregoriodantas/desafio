1f6836b8181a20f3245cb850e6aea12a48563765
claudio.dantas <claudio.dantas@sicoob.com.br>
2020-12-01 15:27:19 -0300

    Retirada do console.log

--- github-react/src/js/reducers/index.js
+++ github-react/src/js/reducers/index.js
@@ -10,12 +10,6 @@ function rootReducer(state = initialState, action) {
 
     switch(action.type){
         case FIND_SUCCESS:
-            console.log("state")
-            console.log(state)
-
-            console.log("action")
-            console.log(action)
-
             return  {...state, repos: action.payload[0]  ? action.payload : []}      
         case FIND_USER_SUCCESS:{
             let repos = [];                
